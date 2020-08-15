62d3cc7f54526edac5eb0b6dc3a105f2bfaa2de6
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-08-15 09:46:43 +0100

    added console.log

--- src/popup/test.vue
+++ src/popup/test.vue
@@ -2,10 +2,18 @@
   <div class="container">
     <p> Message Received {{ message }} </p>
     <p> Error {{ error }} </p>
+    <button
+      @click="receiveMessage"
+    >
+      click Me
+    </button>
   </div> 
 </template>
 
 <style scoped>
+    .container{
+        width: 300px;
+    }
 
 </style>
 
@@ -18,14 +26,20 @@ export default {
             error: ''
         }
     },
+    mounted(){
+        this.receiveMessage()
+    },
     methods: {
         receiveMessage(){
+            console.log('This is the method')
             chrome.runtime.onMessageExternal.addListener(
                 function(request, sender, sendResponse) {
-                    if(request.message){
+                 
                         this.message = request.message
                         this.error = request
-                    }
+                 
+                       console.log('Inside the request and response method')
+                       sendResponse('hello')
                 }
             )
         }
