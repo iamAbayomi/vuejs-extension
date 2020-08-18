ed4645680335f9c356e3bec740182054b3e18fd7
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-08-18 15:38:33 +0100

    added message listener to background

--- src/background.js
+++ src/background.js
@@ -1,3 +1,13 @@
-global.browser = require('webextension-polyfill')
-alert('Hello world!')
+// global.browser = require('webextension-polyfill')
+// alert('Hello world!')
 
+
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse){
+        console.log(request.message)
+        console.log(request)
+        chrome.browserAction.setPopup({ popup: 'popup/App.html' })
+        // text.innerHTML = request.message
+        sendResponse('Search js worked')
+    }
+  )
